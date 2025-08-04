db67c8fc60ef71238683c51b5aedf0d6107d4d61
Suneel Sweety <suneelsweety31@gmail.com>
2025-08-04 16:55:42 +0530

    First Commit

---
+++ src/pages/OurMission.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OurMission = () => {
+  return (
+    <div>Our Mission</div>
+  )
+}
+
+export default OurMission
---
+++ src/services/apis.js
@@ -0,0 +1,23 @@
+
+// ================================
+
+// Authentication API calls - Sujatha & Vamsi
+
+// Course-related API calls - Krishna Priya & Venky
+
+// Lecture-related API calls - Muni
+
+// Payment-related API calls - Satish 
+
+// Career-related API calls - Bhargavi & Suneel
+
+// User-related API calls - 
+
+// Notification-related API calls - 
+
+// Feedback-related API calls -
+
+// Analytics-related API calls -
+
+// Admin-related API calls -
+

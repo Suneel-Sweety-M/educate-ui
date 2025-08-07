3132c97129a7d64e1a4774f816b9c883c5b6131c
harika <harikasuraparaju@gmail.com>
2025-08-07 09:44:52 +0530

    harika-home,program,footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -18,9 +20,18 @@ import About from './pages/About';
 import Careers from './pages/Careers';
 import CareerDetails from './pages/CareerDetails';
 
-function App() {
+import React from 'react';
+
+function AppContent() {
+  const location = useLocation();
+
+  // List of routes where footer should be hidden
+  const hideFooterRoutes = ['/'];
+
+  const hideFooter = hideFooterRoutes.includes(location.pathname);
+
   return (
-    <>
+    <div className="app-container">
       <ToastContainer
         position="top-center"
         autoClose={5000}
@@ -34,26 +45,38 @@ function App() {
         theme="dark"
       />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ChangePassword />} />
-        
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/mission" element={<OurMission />} />
-        <Route path="/course/:courseId" element={<CourseDetails />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/career/:id" element={<CareerDetails />} />
+      <Navbar />
 
-        <Route path="/about-us" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/terms-conditions" element={<Terms />} />
-        <Route path="/privacy-policy" element={<Privacy />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ChangePassword />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/mission" element={<OurMission />} />
+          <Route path="/course/:courseId" element={<CourseDetails />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/career/:id" element={<CareerDetails />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms-conditions" element={<Terms />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+
+      {!hideFooter && <Footer />}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <React.StrictMode>
+      <AppContent />
+    </React.StrictMode>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/home/Hero';
+import CourseCard from '../components/courses/CourseCard';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-page">
+      <Hero />
+      <CourseCard/>
+    </div>
+  );
+};
 
 export default Home;
